e1cc34259ef345c5a8dbe84180e06329562e4f0b
OlegSivets <legolas2002@mail.ru>
2022-07-10 22:29:41 +0300

    token refresh were added

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -17,6 +17,8 @@ export const AuthProvider = ({ children }) => {
       : null
   );
 
+  let [loading, setLoading] = useState(true);
+
   let LoginUser = async e => {
     e.preventDefault();
 
@@ -50,11 +52,42 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem('authTokens');
   };
 
+  let updateToken = async e => {
+    console.log('Update token called');
+    let response = await fetch('http://127.0.0.1:8000/api/token/refresh', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refresh: authTokens.refresh }),
+    });
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem('authTokens', JSON.stringify(data));
+    } else {
+      logoutUser();
+    }
+  };
+
   let contextData = {
     user: user,
     LoginUser: LoginUser,
     logoutUser: logoutUser,
   };
+
+  useEffect(() => {
+    let fourMinutes = 1000 * 60 * 4;
+
+    let interval = setInterval(() => {
+      if (authTokens) {
+        updateToken();
+      }
+    }, fourMinutes);
+    return () => clearInterval(interval);
+  }, [authTokens, loading]);
 
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
