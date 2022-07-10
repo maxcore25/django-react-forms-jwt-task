d37b575bb34ad7bde7405fadd85998622069009f
OlegSivets <legolas2002@mail.ru>
2022-07-11 00:38:29 +0300

    form validation were added

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,14 +1,52 @@
 import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import AuthContext from '../context/AuthContext';
 
 function LoginPage() {
   let { LoginUser } = useContext(AuthContext);
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: 'onBlur',
+  });
+
   return (
     <div>
-      <form onSubmit={LoginUser}>
-        <input type='text' name='username' placeholder='Enter Username' />
-        <input type='password' name='password' placeholder='Enter Password' />
+      <form onSubmit={handleSubmit(LoginUser)}>
+        <label>
+          Name
+          <input
+            {...register('username', {
+              required: 'Enter your name',
+              minLength: {
+                value: 4,
+                message: 'korotkovat',
+              },
+            })}
+          />
+        </label>
+        <div style={{ height: 20 }}>
+          {errors?.username && <p>{errors?.username?.message || 'Error!'}</p>}
+        </div>
+
+        <label>
+          Password
+          <input
+            {...register('password', {
+              required: 'Enter your password',
+              minLength: {
+                value: 5,
+                message: 'korotkovat',
+              },
+            })}
+          />
+        </label>
+        <div style={{ height: 20 }}>
+          {errors?.password && <p>{errors?.password?.message || 'Error!'}</p>}
+        </div>
         <input type='submit' />
       </form>
     </div>
