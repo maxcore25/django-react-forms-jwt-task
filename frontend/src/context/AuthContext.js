eec522833e07acd2887204431471520636ac2819
OlegSivets <legolas2002@mail.ru>
2022-07-11 11:37:45 +0300

    url in UpdateToken function were fixed

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -53,7 +53,7 @@ export const AuthProvider = ({ children }) => {
 
   let updateToken = async e => {
     console.log('Update token called');
-    let response = await fetch('http://127.0.0.1:8000/api/token/refresh', {
+    let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
